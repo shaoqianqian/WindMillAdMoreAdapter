cbfa74b76481b4eaedf4baaf040151a59b3ed01f
邵前前 <>
2025-08-29 16:22:47 +0800

    WindMillAdMoreAdapterSDK

--- pod_trunk.sh
+++ pod_trunk.sh
@@ -1,6 +1,6 @@
 git add .
 git commit -m "WindMillAdMoreAdapterSDK"
 git push origin main
-git tag 1.0.6
+git tag 1.0.7
 git push origin main --tags
 pod trunk push WindMillAdMoreAdapter.podspec --skip-import-validation --skip-tests --allow-warnings
